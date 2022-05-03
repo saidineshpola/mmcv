3231cd4f045245aaeca0d06c6587f130f1508497
saidinesh pola <81302966+saidineshpola@users.noreply.github.com>
2022-05-03 12:45:39 +0530

    Update nms.cpp

--- mmcv/ops/csrc/pytorch/nms.cpp
+++ mmcv/ops/csrc/pytorch/nms.cpp
@@ -1,33 +1,17 @@
-// Copyright (c) OpenMMLab. All rights reserved
 #include "pytorch_cpp_helper.hpp"
 #include "pytorch_device_registry.hpp"
 
-Tensor nms_impl(Tensor boxes, Tensor scores, float iou_threshold, int offset) {
-  return DISPATCH_DEVICE_IMPL(nms_impl, boxes, scores, iou_threshold, offset);
-}
-
-Tensor softnms_impl(Tensor boxes, Tensor scores, Tensor dets,
-                    float iou_threshold, float sigma, float min_score,
-                    int method, int offset) {
-  return DISPATCH_DEVICE_IMPL(softnms_impl, boxes, scores, dets, iou_threshold,
-                              sigma, min_score, method, offset);
-}
-
-std::vector<std::vector<int> > nms_match_impl(Tensor dets,
-                                              float iou_threshold) {
-  return DISPATCH_DEVICE_IMPL(nms_match_impl, dets, iou_threshold);
-}
-
-Tensor nms(Tensor boxes, Tensor scores, float iou_threshold, int offset) {
-  return nms_impl(boxes, scores, iou_threshold, offset);
+Tensor cp_cluster_impl(Tensor boxes, Tensor scores, Tensor dets,
+                       float iou_threshold, float min_score,
+                       int offset, float wfa_thresh, int tune_coords, int opt_id) {
+  return DISPATCH_DEVICE_IMPL(cp_cluster_impl, boxes, scores, dets, iou_threshold,
+                              min_score, offset, wfa_thresh, tune_coords, opt_id);
 }
 
 Tensor softnms(Tensor boxes, Tensor scores, Tensor dets, float iou_threshold,
                float sigma, float min_score, int method, int offset) {
-  return softnms_impl(boxes, scores, dets, iou_threshold, sigma, min_score,
-                      method, offset);
-}
-
-std::vector<std::vector<int> > nms_match(Tensor dets, float iou_threshold) {
-  return nms_match_impl(dets, iou_threshold);
+  //return softnms_impl(boxes, scores, dets, iou_threshold, sigma, min_score,
+  //                    method, offset);
+  return cp_cluster_impl(boxes, scores, dets, iou_threshold, min_score,
+                         offset, 0.8f, 0, 3);
 }
